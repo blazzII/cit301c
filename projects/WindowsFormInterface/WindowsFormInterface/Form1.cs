1fe701e46ace1ca7d6cc001387fb7eff1f4c141e
Jason B. Blazzard <blazzard@gmail.com>
2016-08-24 17:27:10 -0600

    First Commit

---
+++ projects/WindowsFormInterface/WindowsFormInterface/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormInterface
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.label2.Text = "Welcome to the Visual Studio IDE";
+        }
+    }
+}
